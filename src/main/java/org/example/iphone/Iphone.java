4050c13ad61e98e862647a61dfef08005a4315b0
fermarino <119690095+fermarino@users.noreply.github.com>
2024-08-12 20:07:46 -0300

    feat: add iphone class

---
+++ src/main/java/org/example/iphone/Iphone.java
@@ -0,0 +1,64 @@
+package org.example.iphone;
+
+public class Iphone implements MusicPlayer, Phone, Browser{
+
+    @Override
+    public void openUrl(String url) {
+
+    }
+
+    @Override
+    public void refreshPage() {
+
+    }
+
+    @Override
+    public void goBack() {
+
+    }
+
+    @Override
+    public void goForward() {
+
+    }
+
+    @Override
+    public void makeCall(String phoneNumber) {
+
+    }
+
+    @Override
+    public void receiveCall(String phoneNumber) {
+
+    }
+
+    @Override
+    public void sendMessage(String phoneNumber, String message) {
+
+    }
+
+    @Override
+    public void receiveMessage(String phoneNumber, String message) {
+
+    }
+
+    @Override
+    public void playMusic() {
+
+    }
+
+    @Override
+    public void pauseMusic() {
+
+    }
+
+    @Override
+    public void stopMusic() {
+
+    }
+
+    @Override
+    public void setVolume(int volume) {
+
+    }
+}
